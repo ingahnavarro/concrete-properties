7ec4cd7995d1008d44b21bc43a82455cdf268620
ingahnavarro <102553393+ingahnavarro@users.noreply.github.com>
2024-08-04 03:05:54 -0600

    aci318_19

--- src/concreteproperties/design_codes/__init__.py
+++ src/concreteproperties/design_codes/__init__.py
@@ -2,3 +2,5 @@
 
 from concreteproperties.design_codes.as3600 import AS3600
 from concreteproperties.design_codes.nzs3101 import NZS3101
+from concreteproperties.design_codes.aci318_19 import ACI318_19
+
